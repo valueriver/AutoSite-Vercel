e35ebe7476ee312994cc3c3d413cfd1c9a38087f
Your Name <you@example.com>
2024-07-10 14:55:16 +0800

    Update jokes to Chinese

--- api/joke.js
+++ api/joke.js
@@ -1,8 +1,8 @@
 export default async (req, res) => {
     const jokes = [
-        "Why don't scientists trust atoms? Because they make up everything!",
-        "Why did the scarecrow win an award? Because he was outstanding in his field!",
-        "Why don't skeletons fight each other? They don't have the guts."
+        "为什么科学家不相信原子？因为它们组成了一切！",
+        "为什么稻草人会得奖？因为他在田里表现出色！",
+        "为什么骷髅不互相打架？因为他们没有胆量。"
     ];
     const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
     res.status(200).json({ joke: randomJoke });
